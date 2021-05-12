6ba0d5adbab9d1d8abdff7f214ab34749834fbae
Logan Cooper <logancooper@bellsouth.net>
2021-05-12 13:06:04 -0400

    added zombie bonus challenge to RPG game exercise

--- rpg-1.py
+++ rpg-1.py
@@ -1,9 +1,7 @@
 #CLASSES
 class Character:
     def __init__(self, health, power, name):
-        self.health = health
-        self.power = power
-        self.name = name
+        super.__init__(self, health, power, name)
 
     def attack(self, target):
         target.health -= self.power
@@ -32,14 +30,25 @@ class Enemy(Character):
         self.power = power
         self.name = name
 
+class Zombie(Enemy):
+    def __init__(self, health, power, name):
+        self.health = health
+        self.power = power
+        self.name = name
+    
+    def isAlive(self):
+        return True
+
 #VARIABLES
 gandalf = Hero(10,10, 'Gandalf')
 balrog = Enemy(6,2, 'Balrog')
+sauron = Zombie(2,2,'Sauron')
 
 #GLOBAL FUNCTIONS
 def print_start():
         gandalf.print_status()
         balrog.print_status()
+        sauron.print_status()
         print()
         print("What do you want to do?")
         print("1. fight goblin")
@@ -48,12 +57,12 @@ def print_start():
         print("> ",)
 
 def main():
-    while balrog.isAlive() and gandalf.isAlive():
+    while sauron.isAlive() and gandalf.isAlive():
         print_start()
         user_input = input()
         if user_input == "1":
             #hero attacks enemy
-            gandalf.attack(balrog)
+            gandalf.attack(sauron)
         elif user_input == "2":
             pass
         elif user_input == "3":
@@ -62,9 +71,9 @@ def main():
         else:
             print("Invalid input %r" % user_input)
 
-        if balrog.health > 0:
+        if sauron.health > 0:
             # enemy attacks hero
-            balrog.attack(gandalf)
+            sauron.attack(gandalf)
 
 #MAIN
 main()
